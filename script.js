73d2bfc6de30cefe768cde755bb816e184fe7504
marciomazeu <mazeu911@gmail.com>
2025-04-30 21:49:37 -0400

    update all files

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
 
 document.querySelector(".check").addEventListener("click", function () {
-  console.log(document.querySelector(".hideNumber"));
+  console.log(document.querySelector(".hideNumber").value);
 });
