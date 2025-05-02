8906f20e83015cd5c30ca60121222582e922dda9
marciomazeu <mazeu911@gmail.com>
2025-05-01 22:24:30 -0400

    created an effect of color change when the player wins

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 "use strict";
 
+const hideNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".hideNumber").textContent = hideNumber;
+
+let score = 20;
+
 document.querySelector(".check").addEventListener("click", function () {
-  console.log(document.querySelector(".hideNumber").value);
+  const number = Number(document.querySelector(".number").value);
+  console.log(number);
+
+  if (!number) {
+    document.querySelector(".message").textContent = "⛔No number!";
+  } else if (number == hideNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct number!";
+    score++;
+    document.querySelector(".score").textContent = score;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".hideNumber").style.width = "7rem";
+  } else if (number > hideNumber) {
+    if (score > 0) {
+      document.querySelector(".message").textContent = "⬆️ Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+    }
+  } else if (number < hideNumber) {
+    if (score > 0) {
+      document.querySelector(".message").textContent = "⬇️ Too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+    }
+  }
 });
